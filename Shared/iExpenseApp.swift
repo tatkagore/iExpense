5057041e772e23154d079fbd51d989bfcb8352ab
Tatiana Simmer <tatkagore@gmail.com>
2022-07-05 12:31:03 +0200

    Initial Commit

---
+++ Shared/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  Shared
+//
+//  Created by Tatiana Simmer on 05/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
